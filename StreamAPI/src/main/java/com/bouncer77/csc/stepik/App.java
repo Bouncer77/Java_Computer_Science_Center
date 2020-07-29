5ce75382649756dd5d73259690ec75ac753bac5e
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-07-29 16:58:09 +0300

    Stream API

---
+++ StreamAPI/src/main/java/com/bouncer77/csc/stepik/App.java
@@ -0,0 +1,12 @@
+package com.bouncer77.csc.stepik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
